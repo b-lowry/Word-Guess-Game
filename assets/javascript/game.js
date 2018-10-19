124d9598f334a913f6ffa36617898cc9df04529e
Ben <ben.lowry@outlook.comgit config --global user.email ben.lowry@outlook.com>
2018-10-19 16:00:31 -0500

    updated functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,16 @@
+function reset() {
+  wrongAnswerArray = [];
+  document.getElementById("guessed").textContent = wrongAnswerArray;
+  guesses = 10;
+  document.getElementById("remaining").textContent = guesses;
+  word = words[Math.floor(Math.random() *words.length)];
+  answerArray = [];
+  for (var i = 0; i < word.length; i++) {
+    answerArray[i] = "__";
+  }
+  document.getElementById("current-word").textContent = answerArray;
+}
+
 //Define array of words for computer to select one
 var words = ["cowboys", "eagles", "quarterback", "runningback", "chargers", "holding", "penalty", "interception", "fumble", "touchdown", "fieldgoal"];
 
@@ -7,12 +20,13 @@ var word = words[Math.floor(Math.random() * words.length)];
 //Loop through the word and add underscores to equal how many spaces need to be added
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
-  answerArray[i] = " _ ";
+  answerArray[i] = " __ ";
 }
 
 var wrongAnswerArray = [];
 var wins = 0;
 var losses = 0;
+var answer = [];
 
 //Display the spaces on page
 document.getElementById("current-word").textContent = answerArray;
@@ -20,12 +34,6 @@ document.getElementById("current-word").textContent = answerArray;
 //Number of guesses user starts with
 var guesses = 10; 
 document.getElementById("remaining").textContent = guesses;
-
-//create function on keyup "function(event)"
-//store keyup value in var
-//make conditional if letter exists in word or else 
-  //if in word then push to array and display on document
-  //if not in word than push to other array and subtract one of the guesses remaining
 
 document.onkeyup = function(event) {
   var userInput = event.key;
@@ -35,33 +43,30 @@ document.onkeyup = function(event) {
   if(word[i] !== userInput) {
     wrongAnswerArray.push(" " + userInput);
     document.getElementById("guessed").textContent = wrongAnswerArray;
+    guesses --;
+    document.getElementById("remaining").textContent = guesses;
+    if(guesses === 0) {
+      losses ++;
+      document.getElementById("losses").textContent = losses;
+      //function
+      reset();  
+  } 
+};
+
+  if(answerArray.join("") === word) {
+    wins ++;
+    document.getElementById("wins").textContent = wins;
+    reset();
   }
-  
 
   while(i<l){
 
       console.log(word[i],userInput)
-      if (word[i]===userInput){
+      if (word[i] === userInput){
         answerArray[i]=userInput;
         document.getElementById("current-word").textContent = answerArray;
         console.log("hi",answerArray);
       }
       i++ 
-    } 
+    }
   };
-
-
-  // if(word.indexOf(userInput) > -1) {
-  //  //push correct word into the correct place in the answer array
-  //  answerArray.forEach(function() {
-  //    //what goes here to update the answer array at the correct location with the correct letter
-           
-  //  }); 
-  //  document.getElementById("current-word").textContent = answerArray;
-  // } else {
-  //   wrongAnswerArray.push(userInput);
-  //   document.getElementById("guessed").textContent = wrongAnswerArray;
-  //   guesses --; 
-  //   document.getElementById("remaining").textContent = guesses; 
-  // }                    
- 
